d004f341fdde551930ca20d4b080793f55223a06
tguruslan <tguruslan1994@gmail.com>
2019-10-30 02:08:35 +0200

    перероблено під можливість розширення, спрощено код

--- convert.py
+++ convert.py
@@ -2,119 +2,126 @@
 
 import sys
 
-new_f = open(sys.argv[1].replace('.gcode','')+"_new.gcode","w+")
-old_ff = open(sys.argv[1], 'rb')
-old_f = open(sys.argv[1], 'rb')
-new_X = float(sys.argv[2])
-new_Y = float(sys.argv[3])
+# ==============================================================================
+
+
+with open(sys.argv[1]) as file:
+    data = file.read()
+
+# ==============================================================================
+
+
+def sizes(input_data):
+    max_x = 0
+    max_y = 0
+    min_x = 0
+    min_y = 0
+
+    for row in data.split('\n'):
+        for b in row.split():
+            if b.find('X') != -1:
+                x = float(b.replace('X',''))
+                if x > max_x:
+                    max_x = x
+                if x < min_x:
+                    min_x = x
+            if b.find('Y') != -1:
+                y = float(b.replace('Y',''))
+                if y > max_y:
+                    max_y = y
+                if y < min_y:
+                    min_y = y
+
+    return [max_x, max_y, min_x, min_y]
+
+# ==============================================================================
+
+
+def rotate_data(input_data):
+    new_data = ''
+    for row in input_data.split('\n'):
+        new_row = []
+        for b in row.split():
+            if b.find('X') != -1:
+                val = b.replace('X', '')
+                new_row.append('{axis}{val}'.format(axis='Y',val=float(- float(val) + gcode_size[0] - gcode_size[2])))
+            elif b.find('Y') != -1:
+                val = b.replace('Y', '')
+                new_row.append('{axis}{val}'.format(axis='X',val=float(val)))
+            else:
+                new_row.append(b)
+        new_data += ' '.join(new_row)+"\n"
+
+    return new_data
+
+
 rotate = float(sys.argv[4])
+
+if rotate != 0:
+    if rotate == -90:
+        rotate = 270
+    for i in range(int(rotate / 90)):
+        gcode_size = sizes(data)
+        data = rotate_data(data)
+
+# ==============================================================================
+
+
+def morror_xy(input_data, xy):
+    new_data = ''
+    for row in input_data.split('\n'):
+        new_row = []
+        for b in row.split():
+            if b.find('X') != -1:
+                if xy == 'X':
+                    val = b.replace('X', '')
+                    new_row.append('X{val}'.format(val=float(- float(val) + gcode_size[0] - gcode_size[2])))
+                else:
+                    new_row.append(b)
+            elif b.find('Y') != -1:
+                if xy == 'Y':
+                    val = b.replace('Y', '')
+                    new_row.append('Y{val}'.format(val=float(- float(val) + gcode_size[1] - gcode_size[3])))
+                else:
+                    new_row.append(b)
+            else:
+                new_row.append(b)
+        new_data += ' '.join(new_row)+"\n"
+
+    return new_data
+
+
 mirror = sys.argv[5]
 
-max_x = 0
-max_y = 0
-min_x = 0
-min_y = 0
+if mirror != 0:
+    gcode_size = sizes(data)
+    data = morror_xy(data, mirror)
 
-for i in old_ff:
-    for b in i.decode("utf-8").split():
-        if b.find('X') != -1:
-            x = float(b.replace('X',''))
-            if x > max_x:
-                max_x = x
-            if x < min_x:
-                min_x = x
-        if b.find('Y') != -1:
-            y = float(b.replace('Y',''))
-            if y > max_y:
-                max_y = y
-            if y < min_y:
-                min_y = y
+# ==============================================================================
 
-old_ff.close()
 
-def gcode_calc(value, axis):
-    val = float(value.replace(axis,''))
-    if axis == 'X':
-        if mirror == 'X':
-            if rotate == 90:
-                axis = 'Y'
-                new_val = val + new_Y
-            elif rotate == 180:
-                new_val = val + new_X
-            elif rotate == 270 or rotate == -90:
-                axis = 'Y'
-                new_val = - val + max_x - min_x + new_Y
-            elif rotate == 0:
-                new_val = - val + max_x - min_x + new_X
-        elif mirror == 'Y':
-            if rotate == 90:
-                axis = 'Y'
-                new_val = - val + max_x - min_x + new_Y
-            elif rotate == 180:
-                new_val = - val + max_x - min_x + new_X
-            elif rotate == 270 or rotate == -90:
-                axis = 'Y'
-                new_val = val + new_Y
-            elif rotate == 0:
-                new_val = val + new_X
-        else:
-            if rotate == 90:
-                axis = 'Y'
-                new_val = - val + max_x - min_x + new_Y
-            elif rotate == 180:
-                new_val = - val + max_x - min_x + new_X
-            elif rotate == 270 or rotate == -90:
-                axis = 'Y'
-                new_val = val + new_Y
-            elif rotate == 0:
-                new_val = val + new_X
-    else:
-        if mirror == 'X':
-            if rotate == 90:
-                axis = 'X'
-                new_val = val + new_X
-            elif rotate == 180:
-                new_val = - val + max_y - min_y + new_Y
-            elif rotate == 270 or rotate == -90:
-                axis = 'X'
-                new_val = - val + max_y - min_y + new_X
-            elif rotate == 0:
-                new_val = val + new_Y
-        elif mirror == 'Y':
-            if rotate == 90:
-                axis = 'X'
-                new_val = - val + max_y - min_y + new_X
-            elif rotate == 180:
-                new_val = val + new_Y
-            elif rotate == 270 or rotate == -90:
-                axis = 'X'
-                new_val = val + new_X
-            elif rotate == 0:
-                new_val = - val + new_Y + max_y - min_y
-        else:
-            if rotate == 90:
-                axis = 'X'
-                new_val = val + new_X
-            elif rotate == 180:
-                new_val = - val + max_y - min_y + new_Y
-            elif rotate == 270 or rotate == -90:
-                axis = 'X'
-                new_val = - val + max_y - min_y + new_X
-            elif rotate == 0:
-                new_val = val + new_Y
+def move_xy(input_data, x, y):
+    new_data = ''
+    for row in input_data.split('\n'):
+        new_row = []
+        for b in row.split():
+            if b.find('X') != -1:
+                new_row.append('X{new_x}'.format(new_x=float(float(b.replace('X', '')) + x)))
+            elif b.find('Y') != -1:
+                new_row.append('Y{new_y}'.format(new_y=float(float(b.replace('Y', '')) + y)))
+            else:
+                new_row.append(b)
+        new_data += ' '.join(new_row)+"\n"
+    return new_data
 
-    return '{ax}{c}'.format(ax=axis, c=new_val)
 
-for a in old_f:
-    new_row = []
-    for b in a.decode("utf-8").split():
-        if b.find('X') != -1:
-            new_row.append(gcode_calc(b, 'X'))
-        elif b.find('Y') != -1:
-            new_row.append(gcode_calc(b, 'Y'))
-        else:
-            new_row.append(b)
-    new_f.write(' '.join(new_row)+"\n")
+new_X = float(sys.argv[2])
+new_Y = float(sys.argv[3])
 
-old_f.close()
-new_f.close()
+if new_X != 0 or new_Y != 0:
+    data = move_xy(data, new_X, new_Y)
+
+# ==============================================================================
+
+with open(sys.argv[1].replace('.gcode','')+"_new.gcode", 'w') as file:
+    file.write(data)
